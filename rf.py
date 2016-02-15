8e199a9a0c9d75d7c984a5e6249967a41fde2ecc
shuckle16 <shuckle16@users.noreply.github.com>
2016-02-15 16:55:35 -0500

    Update rf.py

--- rf.py
+++ rf.py
@@ -3,7 +3,6 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.cross_validation import train_test_split
-from sklearn.metrics import classification_report
 from sklearn.metrics import roc_curve
 from sklearn.metrics import roc_auc_score
 
@@ -20,12 +19,25 @@ X.replace(to_replace=' *\?', value=-1, regex=True, inplace=True)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-rf = RandomForestClassifier(n_estimators = 20,n_jobs=2)
 
-rf.fit(X_train,y_train)
-predrf = rf.predict(X_test)
-print(classification_report(y_test, predrf))
+aucs = []
 
-print pd.crosstab(pd.core.series.Series(y_test), predrf, rownames=['actual'], colnames=['preds'])
+trees = [2,5,10,25,500]
 
-print roc_auc_score(y_test,predrf)
+for tree in trees:
+    rf = RandomForestClassifier(n_estimators = tree,n_jobs=2)
+
+    rf.fit(X_train,y_train)
+    predrf = rf.predict(X_test)
+
+    #print pd.crosstab(pd.core.series.Series(y_test), predrf, rownames=['actual'], colnames=['preds'])
+    
+    scores = cross_val_score(rf, X_train, y_train, scoring='roc_auc',cv=5)
+    print(tree,"aucs: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+    
+    aucs.append(roc_auc_score(y_test,predrf))
+    #print roc_auc_score(y_test,predrf)
+
+
+plt.plot(trees,aucs)
